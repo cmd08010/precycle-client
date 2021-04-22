79a7a63284f1fd438249bdaa54e2dc9e049ace37
cmd08010 <colleendunion@gmail.com>
2021-04-22 13:14:24 -0400

    fix spelling in div

--- src/components/Header/Home.js
+++ src/components/Header/Home.js
@@ -11,38 +11,38 @@ const Home = ({ user, msgAlert }) => {
   const deleteItem = (item) => {
     console.log(item)
     deleteItem(user, item.id)
-    .then(res => console.log(res))
+      .then(res => console.log(res))
   }
 
 
   return (
     <div className="container">
-    <div className="row">
-      <div className="col-sm-10 col-md-8 mx-auto mt-5">
-      {!user &&
-        <div className="home-page">
-          <div className="header-3">Check out (p)recycle!</div>
-          <div className="header-5">
-            <Link to="/sign-in">Sign-in</Link> or <Link to="sign-up">Sign-up</Link> to get started!
+      <div className="row">
+        <div className="col-sm-10 col-md-8 mx-auto mt-5">
+          {!user &&
+            <div className="home-page">
+              <div className="header-3">Check out (p)recycle!</div>
+              <div className="header-5">
+                <Link to="/sign-in">Sign-in</Link> or <Link to="sign-up">Sign-up</Link> to get started!
           </div>
-            <img src="Recycle.jpeg" />
+              <img src="Recycle.jpeg" />
+            </div>
+          }
+          {user && <div className="home-page">
+            <div className="home-page">
+              <div className="header-4">Welcome, {user.email}</div>
+              <div className="home-page-message">
+                <img src="Recycle.jpeg" />
+                <p>Precycle is an application that allows users to find out if items are recyclable or not. Check out our features by navigating to
+                Scan an Item. You can scan the barcode with your camera, type in a keyword or name of an item, or enter the numerical Barcode
+            to find out if your item is recyclable. The items are being updated constantly so keep checking back!</p>
+              </div>
+            </div>
           </div>
-        }
-        { user && <div className="home-page">
-          <div className="home-page">
-          <div className="header-4">Welcome, {user.email}</div>
-          <div className="home-page-message">
-          <img src="Recycle.jpeg" />
-            <p>Precycle is an application that allows users to find out if items are recycleable or not. Check out our features by navigating to
-            Scan an Item. You can scan the barcode with your camera, type in a keyword or name of an item, or enter the numerical Barcode
-            to find out if your item is recycleable. The items are being updated constantly so keep checking back!</p>
-          </div>
-          </div>
-          </div>
-        }
+          }
+        </div>
+      </div>
     </div>
-    </div>
-  </div>
   )
 }
 
